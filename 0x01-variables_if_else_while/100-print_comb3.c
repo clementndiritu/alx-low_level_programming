fe02b348189a01e00f50d98ee60e32e9fe6cfe3c
clementndiritu <clemndiritu@gmail.com>
2022-03-11 18:05:52 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,50 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * main - main block
- * Description: Write a program that prints all possible
- * different combinations of two digits
- * Numbers must be separated by ,, followed by a space
- * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Print only the smallest combination of two digits
- * Numbers should be printed in ascending order, with two digits
- * You can only use the putchar function
- * (every other function (printf, puts, etc…) is forbidden)
- * You can only use putchar five times maximum in your code
- * You are not allowed to use any variable of type char
- * All your code should be in the main function
+ * main - prints out all the numbers between 00 and 99
+ * with no two digits being the same
  * Return: 0
  */
 int main(void)
 {
-	int c;
-	int d = 0;
+	int i, j;
 
-	while (d < 10)
+	for (i = 48; i < 58; i++)
 	{
-		c = 0;
-		while (c < 10)
+		for (j = i; j < 58; j++)
 		{
-			if (d != c && d < c)
+			if (i == j)
 			{
-				{
-					putchar('0' + d);
-					putchar('0' + c);
-
-					if (c + d != 17)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-
-				c++;
+				continue;
 			}
-			d++;
-		}
-		putchar('\n');
-		return (0);
-	}
-}
 
+			putchar(i);
+			putchar(j);
+
+			if (i == 56 && j == 57)
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
