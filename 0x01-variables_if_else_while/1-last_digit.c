4a008d260a1410cc485d9773e086a94b770937a0
clementndiritu <clemndiritu@gmail.com>
2022-03-11 02:24:31 -0800

    t-1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints the alphabet in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
