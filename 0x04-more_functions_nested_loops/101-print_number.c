5fcac89920a4e3db26cb6074a9dc64b5e95a28d4
clementndiritu <clemndiritu@gmail.com>
2022-03-17 03:14:31 -0700

    t-101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_number - prints numbers
+ * @n: number to be printed
+ * Return:void
+ */
+
+void print_number(int n)
+{
+
+	int d = 1, i = 0, ii = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	while (n / d != 0)
+	{
+		d *= 10;
+		i++;
+	}
+
+	if (i == 0)
+		_putchar('0' + n);
+
+}
