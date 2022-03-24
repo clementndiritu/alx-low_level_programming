2030d969966350897ae73683ef0f0a2e68fc855d
clementndiritu <clemndiritu@gmail.com>
2022-03-24 02:28:52 -0700

    t-8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string
+ * Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i])
+	{
+		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] >= 'a' && s[i] < 'n') || (s[i] >= 'A' && s[i] < 'N'))
+			{
+				s[i] = s[i] + 26;
+			}
+			s[i] = s[i] - 13;
+			break;
+		}
+		i++;
+	}
+	return (s);
+}
